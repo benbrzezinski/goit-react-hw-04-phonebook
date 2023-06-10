30ba9158d43a360e1f0440af32927f88fd03b700
Beniamin Brzeziński <benek9311@wp.pl>
2023-06-10 12:31:05 +0200

    Modified state in Form

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -10,14 +10,13 @@ const ID = {
 };
 
 const Form = ({ contacts, addContact }) => {
-  const [name, setName] = useState('');
-  const [number, setNumber] = useState('');
+  const [values, setValues] = useState({ name: '', number: '' });
 
   const handleChange = e => {
     const { name, value } = e.currentTarget;
 
-    if (name === 'name') setName(value);
-    if (name === 'number') setNumber(value);
+    if (name === 'name') setValues({ ...values, name: value });
+    if (name === 'number') setValues({ ...values, number: value });
   };
 
   const handleSubmit = e => {
@@ -25,19 +24,18 @@ const Form = ({ contacts, addContact }) => {
 
     const contactsNames = contacts.map(({ name }) => name);
 
-    if (contactsNames.includes(name)) {
-      return toast.error(`${name} is already in contacts!`);
+    if (contactsNames.includes(values.name)) {
+      return toast.error(`${values.name} is already in contacts!`);
     }
 
     const newContact = {
       id: nanoid(),
-      name,
-      number,
+      name: values.name,
+      number: values.number,
     };
 
     addContact(newContact);
-    setName('');
-    setNumber('');
+    setValues({ name: '', number: '' });
   };
 
   return (
@@ -50,7 +48,7 @@ const Form = ({ contacts, addContact }) => {
         id={ID.nameInput}
         type="text"
         name="name"
-        value={name}
+        value={values.name}
         onChange={handleChange}
         pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
         title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
@@ -64,7 +62,7 @@ const Form = ({ contacts, addContact }) => {
         id={ID.numberInput}
         type="tel"
         name="number"
-        value={number}
+        value={values.number}
         onChange={handleChange}
         pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
         title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
